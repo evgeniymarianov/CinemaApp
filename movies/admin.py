41fba5940250d4ed2176a8932073612b540eddbf
evgeniymarianov <evgeniymarianov@gmail.com>
2020-06-26 22:33:50 +0300

    Update Admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,16 @@
+from django import forms
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from modeltranslation.admin import TranslationAdmin
 
-# Register your models here.
+from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
+
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(MovieShots)
+admin.site.register(Actor)
+admin.site.register(Rating)
+admin.site.register(RatingStar)
+admin.site.register(Reviews)
