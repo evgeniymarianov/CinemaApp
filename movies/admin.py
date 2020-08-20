f1ff2676b66cdf320d1395b7771279a012bf1ba3
evgeniymarianov <evgeniymarianov@gmail.com>
2020-08-21 02:23:21 +0300

    Добавил классы в админ-панель

--- movies/admin.py
+++ movies/admin.py
@@ -53,14 +53,41 @@ class MovieAdmin(admin.ModelAdmin):
 
 @admin.register(Reviews)
 class ReviewAdmin(admin.ModelAdmin):
-    """Фильмы"""
+    """Отзывы"""
     list_display = ("name", "email", "parent", "movie", "id")
     readonly_fields = ("name", "email")
 
-# admin.site.register(Category, CategoryAdmin)
-admin.site.register(Genre)
-admin.site.register(MovieShots)
-admin.site.register(Actor)
-admin.site.register(Rating)
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    """Жанры"""
+    list_display = ("name", "url")
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    """Актеры"""
+    list_display = ("name", "age")
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    """Рейтинг"""
+    list_display = ("movie", "ip", "star")
+
+
+@admin.register(MovieShots)
+class MovieShotsAdmin(admin.ModelAdmin):
+    """Кадры из фильма"""
+    list_display = ("title", "movie")
+
+
 admin.site.register(RatingStar)
+
+# admin.site.register(Category, CategoryAdmin)
+#admin.site.register(Genre)
+#admin.site.register(MovieShots)
+#admin.site.register(Actor)
+#admin.site.register(Rating)
+#admin.site.register(RatingStar)
 # admin.site.register(Reviews)
