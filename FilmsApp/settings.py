4e5d7a01926da32347598d43256df01008c8e05d
evgeniymarianov <evgeniymarianov@gmail.com>
2020-08-21 01:36:26 +0300

    Упаковал в Докер

--- FilmsApp/settings.py
+++ FilmsApp/settings.py
@@ -78,12 +78,18 @@ WSGI_APPLICATION = 'FilmsApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
--- movies/admin.py
+++ movies/admin.py
@@ -6,9 +6,22 @@ from django.contrib import admin
 
 from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
 
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "url")
+    list_display_links = ("name",)
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    """Фильмы"""
+    list_display = ("title", "category", "url", "draft")
+    list_filter = ("category", "year")
+    search_fields = ("title", "category__name")
+
+
+# admin.site.register(Category, CategoryAdmin)
 admin.site.register(Genre)
-admin.site.register(Movie)
 admin.site.register(MovieShots)
 admin.site.register(Actor)
 admin.site.register(Rating)
