a0c9fabbd5da02c73420ec7b0a7e0374e8e21be4
evgeniymarianov <evgeniymarianov@gmail.com>
2020-08-21 02:15:51 +0300

    Поправил админ-панель

--- movies/admin.py
+++ movies/admin.py
@@ -12,13 +12,50 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display_links = ("name",)
 
 
+class ReviewInline(admin.TabularInline):
+    model = Reviews
+    extra = 1
+    readonly_fields = ("name", "email")
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     """Фильмы"""
     list_display = ("title", "category", "url", "draft")
     list_filter = ("category", "year")
     search_fields = ("title", "category__name")
+    inlines = [ReviewInline]
+    save_on_top = True
+    save_as = True
+    list_editable = ("draft",)
+    # fields = (("actors", "directors", "genres"), )
+    fieldsets = (
+        (None, {
+            "fields": (("title", "tagline"),)
+        }),
+        (None, {
+            "fields": ("description", "poster")
+        }),
+        (None, {
+            "fields": (("year", "world_premiere", "country"),)
+        }),
+        ("Actors", {
+            "classes": ("collapse",),
+            "fields": (("actors", "directors", "genres", "category"),)
+        }),
+        (None, {
+            "fields": (("budget", "fees_in_usa", "fess_in_world"),)
+        }),
+        ("Options", {
+            "fields": (("url", "draft"),)
+        }),
+    )
 
+
+@admin.register(Reviews)
+class ReviewAdmin(admin.ModelAdmin):
+    """Фильмы"""
+    list_display = ("name", "email", "parent", "movie", "id")
+    readonly_fields = ("name", "email")
 
 # admin.site.register(Category, CategoryAdmin)
 admin.site.register(Genre)
@@ -26,4 +63,4 @@ admin.site.register(MovieShots)
 admin.site.register(Actor)
 admin.site.register(Rating)
 admin.site.register(RatingStar)
-admin.site.register(Reviews)
+# admin.site.register(Reviews)
